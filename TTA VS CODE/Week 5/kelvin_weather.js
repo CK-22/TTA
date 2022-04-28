40c3a3ae3c03648f2c73b68b915dca5671385054
CK-22 <102611313+CK-22@users.noreply.github.com>
2022-04-28 23:53:21 +0100

    Add files via upload

---
+++ TTA VS CODE/Week 5/kelvin_weather.js
@@ -0,0 +1,5 @@
+const kelvin = 293;
+var celsius = kelvin - 273
+var fahrenheit = celsius*(9/5)+32
+const fahrenheit = Math.floor(celsius*(9/5)+32)
+console.log('The Temperature is ${fahrenheit} degrees Fahrenheit.');
