f050cc194c33369ad1627ef5cfdaea8339a48061
CK-22 <102611313+CK-22@users.noreply.github.com>
2022-04-30 02:41:08 +0100

    Delete TTA VS CODE/Week 5 directory

--- TTA VS CODE/Week 5/kelvin_weather.js
+++
@@ -1,5 +0,0 @@
-const kelvin = 293;
-var celsius = kelvin - 273
-var fahrenheit = celsius*(9/5)+32
-const fahrenheit = Math.floor(celsius*(9/5)+32)
-console.log('The Temperature is ${fahrenheit} degrees Fahrenheit.');
