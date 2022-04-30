eaa8dc0ab15beaf74ffda9002a878f378b1c8a02
CK-22 <102611313+CK-22@users.noreply.github.com>
2022-04-30 02:43:31 +0100

    Update index.js

--- TTA VS CODE/Week 5/kelvin-weather/index.js
+++ TTA VS CODE/Week 5/kelvin-weather/index.js
@@ -7,5 +7,5 @@ let fahrenheit = celsius*(9/5)+32;
 //will give decimal so .floor will round down
 fahrenheit = Math.floor(fahrenheit);
 
-/* sting templates need ti be wrapped in backticks `` not "" or ''*/
+/* string templates need to be wrapped in backticks `` not "" or ''*/
 console.log(`The Temperature is ${fahrenheit} degrees Fahrenheit.`);
